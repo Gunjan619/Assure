2987413ca57fc3c4fd4dab190fc704cf5a9a23a1
Gunjan Sharma <gunjansharma0619@gmail.com>
2024-09-26 12:00:41 +0530

    Update image asset references and modify splash screen gradient

---
+++ lib/Basic.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+
+const kSpacing = 16.0;
+const kPrimaryColor = Color(0xFFE044B9);
+const kSecondaryColor = Color(0xFFF7A15F);
+const kTextColor = Colors.black; // Darker color for better contrast
+
+class BasicInformationPage extends StatefulWidget {
+  const BasicInformationPage({Key? key}) : super(key: key);
+
+  @override
+  State<BasicInformationPage> createState() => _BasicInformationPageState();
+}
+
+class _BasicInformationPageState extends State<BasicInformationPage> {
+  String? selectedValue;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Container(
+            height: MediaQuery.of(context).size.height,  // Ensure the container takes the full height
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [kPrimaryColor, kSecondaryColor],
+              ),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(kSpacing),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: kSpacing),
+                  Center(
+                    child: Column(
+                      children: [
+                        // Adding the logo above the avatar
+                        Container(
+                          width: MediaQuery.of(context).size.width,  // Full width of the screen
+                          height: 150,  // Adjust the height as per your preference
+                          child: Image.asset(
+                            'Images/logo.png', // Replace with your image path
+                            fit: BoxFit.contain,  // Contain ensures the image fits within the given width and height while maintaining its aspect ratio
+                          ),
+                        ),
+                        const SizedBox(height: kSpacing),
+                        CircleAvatar(
+                          radius: 80.0,
+                          backgroundColor: Colors.white,
+                          child: ClipOval(
+                            child: Image.asset(
+                              'Images/Avatar.png',
+                              fit: BoxFit.cover, // Ensures the avatar fits the circle
+                              width: 160.0,
+                              height: 160.0,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: kSpacing),
+
+                  const SizedBox(height: kSpacing * 2),
+                  Text(
+                    'What is your full name?',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                      color: kTextColor,
+                    ),
+                  ),
+                  const SizedBox(height: kSpacing),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      fillColor: Colors.white.withOpacity(0.42),
+                      filled: true,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      contentPadding: const EdgeInsets.all(16.0),
+                    ),
+                  ),
+                  const SizedBox(height: kSpacing),
+                  Text(
+                    'Who are you?',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                      color: kTextColor,
+                    ),
+                  ),
+                  const SizedBox(height: kSpacing),
+                  DropdownButtonFormField<String>(
+                    value: selectedValue,
+                    onChanged: (newValue) {
+                      setState(() {
+                        selectedValue = newValue;
+                      });
+                    },
+                    items: const [
+                      DropdownMenuItem(
+                        value: 'Father',
+                        child: Text('Father'),
+                      ),
+                      DropdownMenuItem(
+                        value: 'Mother',
+                        child: Text('Mother'),
+                      ),
+                    ],
+                    decoration: InputDecoration(
+                      fillColor: Colors.white.withOpacity(0.42),
+                      filled: true,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      contentPadding: const EdgeInsets.all(16.0),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/Home.dart
@@ -0,0 +1,137 @@
+import 'package:assure/Basic.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'MobileLoginPage.dart';
+
+class SignInPage extends StatelessWidget {
+  const SignInPage({Key? key}) : super(key: key);
+
+  Future<User?> _signInWithGoogle() async {
+    try {
+      // Create a GoogleSignIn object
+      final GoogleSignIn _googleSignIn = GoogleSignIn();
+
+      // Clear the default account to force account selection
+      await _googleSignIn.signOut();
+
+      // Trigger the Google Authentication flow
+      final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+
+      if (googleUser != null) {
+        // Obtain the Google authentication details
+        final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+
+        // Create a new credential
+        final credential = GoogleAuthProvider.credential(
+          accessToken: googleAuth.accessToken,
+          idToken: googleAuth.idToken,
+        );
+
+        // Sign in to Firebase with the Google credential
+        UserCredential userCredential = await FirebaseAuth.instance.signInWithCredential(credential);
+        return userCredential.user;
+      }
+    } catch (e) {
+      print("Error signing in with Google: $e");
+      return null;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        padding: const EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            // Logo section
+            Column(
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width,  // Full width of the screen
+                  height: 150,  // Adjust the height as per your preference
+                  child: Image.asset(
+                    'Images/logo.png', // Replace with your image path
+                    fit: BoxFit.contain,  // Contain ensures the image fits within the given width and height while maintaining its aspect ratio
+                  ),
+                ),
+                SizedBox(height: 10),
+              ],
+            ),
+            const SizedBox(height: 30),
+
+            // Illustration section
+            Image.asset(
+              'Images/openImage.png', // Replace with your illustration image
+              width: 250, // Adjust based on the image size
+              height: 250,
+              fit: BoxFit.contain,
+            ),
+
+            const SizedBox(height: 30),
+
+            // Google Sign-in button
+            GestureDetector(
+              onTap: () async {
+                User? user = await _signInWithGoogle();
+                if (user != null) {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => const BasicInformationPage()),
+                  );
+                } else {
+                  // Handle sign-in failure
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text("Google sign-in failed")),
+                  );
+                }
+              },
+              child: Container(
+                padding: const EdgeInsets.symmetric(vertical: 12.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const SizedBox(width: 10),
+                    ElevatedButton.icon(
+                      onPressed: null,
+                      label: Text('Sign in with Google'),
+                      icon: Icon(Icons.login),
+                      style: ElevatedButton.styleFrom(
+                        iconColor: Colors.white,
+                        side: BorderSide(color: Colors.grey),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 20),
+
+            // Mobile number sign-in option
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => BabyCareWelcomeScreen()),
+                );
+              },
+              child: const Text(
+                'or continue with Mobile Number',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.blueAccent,
+                  decoration: TextDecoration.underline,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
